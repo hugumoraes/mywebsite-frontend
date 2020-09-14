04d301e5ea0d3fb3b2957a67ef5145bae17ff1f9
Hugo Moraes Bonatto <hugomoraes_@hotmail.com>
2020-09-14 17:54:12 -0300

    blog updates

--- src/pages/Blogpost/index.js
+++ src/pages/Blogpost/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
 import { format, parseISO } from 'date-fns';
 import pt from 'date-fns/locale/pt-BR';
 
@@ -7,6 +8,27 @@ import { Container, Post } from './styles';
 import Header from '../../components/Header';
 
 export default function Blogpost({ data }) {
+  const maxWidth = window.innerWidth;
+
+  const renderers = {
+    image: ({
+      alt,
+      src,
+      title,
+    }: {
+      alt?: string,
+      src?: string,
+      title?: string,
+    }) => (
+      <img
+        alt={alt}
+        src={src}
+        title={title}
+        style={{ maxWidth: Math.min(420, maxWidth) }}
+      />
+    ),
+  };
+
   const date = parseISO(data.createdAt);
 
   return (
@@ -20,6 +42,11 @@ export default function Blogpost({ data }) {
             locale: pt,
           })}
         </span>
+        <ReactMarkdown
+          source={data.Content}
+          escapeHtml={false}
+          renderers={renderers}
+        />
       </Post>
     </Container>
   );
